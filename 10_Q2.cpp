833c5f27b2834555fb751c031c4bcd28047a73ea
Manish Tripathi <manish82644@gmail.com>
2024-06-17 00:14:22 +0530

    Refactor code to display the sum of odd digits in reverse order

--- 10_Q2.cpp
+++ 10_Q2.cpp
@@ -8,8 +8,10 @@ int main()
 
     int number, sum = 0, lastDigit;
 
-    cout << "Enter Number: " << endl;
+    cout << "Enter Number: ";
     cin >> number;
+
+    cout << "Odd Digits in reverse order: ";
 
     while (number > 0)
     {
@@ -18,12 +20,14 @@ int main()
         // What this do : if number is odd then perform its sum
         if (lastDigit % 2 != 0)
         {
+            cout << lastDigit << ", ";
             sum += lastDigit;
         }
+
         number /= 10;
     }
 
-    cout << sum << endl;
+    cout << "\nSum of Odd Digits: " << sum << endl;
 
     return 0;
 }
